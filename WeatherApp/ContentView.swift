4c4431bbe2a20ef187101ed96d5760cdf73a0969
Rob <rdaly1490@gmail.com>
2020-07-27 22:34:46 -0400

    Initial Commit

---
+++ WeatherApp/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  WeatherApp
+//
+//  Created by Rob on 7/27/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!").padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ WeatherApp/WeatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAppApp.swift
+//  WeatherApp
+//
+//  Created by Rob on 7/27/20.
+//
+
+import SwiftUI
+
+@main
+struct WeatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
